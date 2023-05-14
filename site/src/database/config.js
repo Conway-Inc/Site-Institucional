47727c9830095ec69a06f8c7e1d3a0ccdc050942
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2023-05-14 20:45:59 -0300

    db configurado

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,7 +20,7 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "127.0.0.1",
-    database: "acquatec",
+    database: "buswayDb",
     user: "urubu100",
     password: "urubu100",
 };
