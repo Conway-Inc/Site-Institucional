6fd2acfe72b31d2e2c36058e6b9d63c86eb7f1bc
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2023-05-19 16:34:53 -0300

    alteracoes menuDashboard

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -105,7 +105,54 @@ function cadastrarRepresentante(req, res) {
         res.status(500).json(erro.sqlMessage);
       });
   }
+
+}function alterarFuncionario(req, res) {
+  // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+  var id = req.body.idFuncionarioServer
+  var nome = req.body.nomeServer;
+  var email = req.body.emailServer;
+  var senha = req.body.senhaServer;
+  var cpf = req.body.cpfServer;
+  var celular = req.body.celularServer;
+  var fotoRepresentante = req.body.fotoRepresentanteServer;
+  var fkEmpresa = req.body.fkEmpresaServer;
+
+  // Faça as validações dos valores
+  if (nome == undefined) {
+    res.status(400).send("Seu nome está undefined!");
+  } else if (email == undefined) {
+    res.status(400).send("Seu email está undefined!");
+  } else if (senha == undefined) {
+    res.status(400).send("Sua senha está undefined!");
+  } else if (cpf == undefined) {
+    res.status(400).send("Seu cpf está undefined!");
+  } else {
+    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+    usuarioModel
+      .alterarFuncionario(
+        id,
+        nome,
+        email,
+        senha,
+        cpf,
+        celular,
+        fotoRepresentante,
+        fkEmpresa
+      )
+      .then(function (resultado) {
+        res.json(resultado);
+      })
+      .catch(function (erro) {
+        console.log(erro);
+        console.log(
+          "\nHouve um erro ao realizar o cadastro! Erro: ",
+          erro.sqlMessage
+        );
+        res.status(500).json(erro.sqlMessage);
+      });
+  }
 }
+
 function cadastrarEmpresa(req, res) {
   // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
   var cnpj = req.body.cnpjServer;
@@ -228,6 +275,7 @@ module.exports = {
   cadastrarRotas,
   capturarIdEmpresa,
   cadastrarEmpresa,
+  alterarFuncionario,
   listar,
   testar,
 };
--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -12,7 +12,11 @@ function listar() {
 }
 
 function entrar(email, senha) {
-  console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
+  console.log(
+    "ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ",
+    email,
+    senha
+  );
   var instrucao = `
       SELECT * FROM Funcionario WHERE email = '${email}' AND senha = '${senha}';
   `;
@@ -21,7 +25,15 @@ function entrar(email, senha) {
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-function cadastrarRepresentante(nome, email, senha, cpf, celular, foto, idEmpresa) {
+function cadastrarRepresentante(
+  nome,
+  email,
+  senha,
+  cpf,
+  celular,
+  foto,
+  idEmpresa
+) {
   console.log(
     "ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():",
     nome,
@@ -66,7 +78,6 @@ function cadastrarFuncionario(
   celular,
   foto
 ) {
-
   // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
   //  e na ordem de inserção dos dados.
   var instrucao = `
@@ -77,11 +88,53 @@ function cadastrarFuncionario(
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function alterarFuncionario(id, nome, email, senha, celular, foto) {
+  var instrucao = `UPDATE Funcionario SET nome = ${nome}
+                  AND email = ${email} 
+                  AND senha = ${senha}
+                  AND celular = ${celular}
+                  AND ${foto}
+                  WHERE idFuncionario = ${id}`;
+
+  console.log("Executando a instrução SQL: \n" + instrucao);
+  return database.executar(instrucao);
+}
+
 module.exports = {
   entrar,
   cadastrarRepresentante,
   listar,
   capturarIdEmpresa,
   cadastrarEmpresa,
-  cadastrarFuncionario
+  cadastrarFuncionario,
+  alterarFuncionario
 };
