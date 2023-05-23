b9f3abf7b1828cb1202635f33b087d4c7f4446f2
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2023-05-22 21:32:18 -0300

    adicionado script de insercao de modelos de veiculos

--- sql/script_teste.sql
+++ sql/script_teste.sql
@@ -7,7 +7,6 @@ INSERT INTO Funcionario VALUES (1, '12345678901', 'Alexandra', 'alexandra@conway
 							   (5, '12345678905', 'Felipe', 'felipe@conway.com', '12345', '11940028923', 1, 1, 'felipe.jpg'),
 							   (6, '12345678906', 'Danielle', 'danielle@conway.com', '12345', '11940028923', 1, 5, 'danielle.jpg');
                                
-INSERT INTO Modelo VALUES (1, 'Padron', 8.5, 60, 1, 1);
 INSERT INTO Veiculo VALUES (1, 'ABC123', 2002, 1, 1);
 
 INSERT INTO Ponto VALUES (1, '01414000', 'Rua Haddock Lobo', 595, null, null);
