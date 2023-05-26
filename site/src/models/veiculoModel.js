a3009e4487145e75a03de7abac245835dd29d4b1
felipeguerrino-sptech <felipe.guerrino@sptech.school>
2023-05-26 16:43:02 -0300

    alterar veiculo incompleto

--- site/src/models/veiculoModel.js
+++ site/src/models/veiculoModel.js
@@ -10,6 +10,17 @@ function cadastrarVeiculo(placaVeiculo, anoAquisicao, fkModelo, fkEmpresa) {
   return database.executar(instrucao);
 }
 
+function alterarVeiculo(idVeiculo, placa, anoAquisicao, fkModelo) {
+  var instrucao = `
+    UPDATE Veiculo SET placa = ${placa},
+                       anoAquisicao = ${anoAquisicao},
+                       fkModelo = ${fkModelo}
+                       WHERE idVeiculo = ${idVeiculo};
+  `
+  console.log("Executando a instrução SQL: \n" + instrucao);
+  return database.executar(instrucao);
+}
+
 function listar(fkEmpresa) {
   console.log(
     "ACESSEI O AVISO  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()"
