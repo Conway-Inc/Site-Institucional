c3f699342d5a7aed07ff6b6d84ad7444252c6638
brunolimabh <bruno.lima@sptech.school>
2023-10-03 12:06:16 -0300

    refactor: Exibindo totens de acorod com o estado

--- siteConWay/src/controllers/graficoBrunoController.js
+++ siteConWay/src/controllers/graficoBrunoController.js
@@ -1,6 +1,21 @@
 var graficoBrunoModel = require("../models/graficoBrunoModel");
 
+function exibirTotensEstado(req, res) {
+  var estado = req.params.estado;
+
+  graficoBrunoModel.exibirTotensEstado(estado).then(function (resultado) {
+    if (resultado.length > 0) {
+      res.status(200).json(resultado);
+    } else {
+      res.status(204).send("Nenhum resultado encontrado!")
+    }
+  }).catch(function (erro) {
+    console.log(erro);
+    console.log("Houve um erro ao buscar os avisos: ", erro.sqlMessage);
+    res.status(500).json(erro.sqlMessage);
+  });
+}
 
 module.exports = {
-  
+  exibirTotensEstado
 };
--- siteConWay/src/models/graficoBrunoModel.js
+++ siteConWay/src/models/graficoBrunoModel.js
@@ -1,7 +1,17 @@
 var database = require("../database/config");
 
-
+function exibirTotensEstado(estado) {
+    console.log(
+      "Acessei o grafiBrunoModel e executei a função exibirTotensEstado(): ",
+      estado
+    );
+    var instrucao = `
+    SELECT * FROM vw_totem_estado WHERE estado = '${estado}';
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucao);
+    return database.executar(instrucao);
+  }
 
 module.exports = {
-  
+  exibirTotensEstado
 };
