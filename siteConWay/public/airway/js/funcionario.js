a09fa4b072053e078da180e313f57f3dbf32bb71
KauanCursino <kauan.souza@sptech.school>
2023-10-03 13:59:01 -0300

    refactor(Perfil): API do CNPJ funcionando na página de perfil

--- siteConWay/public/airway/js/funcionario.js
+++ siteConWay/public/airway/js/funcionario.js
@@ -91,6 +91,7 @@ function exibirInfosFunc(){
     }
     function exibirInfosEmpresa(fkEmpresaVar){
         var fkEmpresaVar = sessionStorage.FK_EMPRESA
+        ipt_telefoneEmpresa.value = sessionStorage.TELEFONE_EMPRESA;
     
         fetch(`/empresa/exibirInfosEmpresa/${fkEmpresaVar}`)
             .then(function (resposta) {
@@ -101,6 +102,7 @@ function exibirInfosFunc(){
                         console.log(json);
                         var infosCnpjEmpresa = document.getElementById("ipt_cnpjEmpresa")
                         infosCnpjEmpresa.value = json[0].cnpj
+                        obterInfosEmpresa()
                     });
                 } else {
                     resposta.text().then(texto => {
@@ -112,4 +114,43 @@ function exibirInfosFunc(){
             });
         return false;
         
+}
+function obterInfosEmpresa() {
+    fetch(`https://publica.cnpj.ws/cnpj/${ipt_cnpjEmpresa.value}`)
+        .then(data => {
+            return data.json();
+        })
+        .then(post => {
+
+            dadosCNPJ = post;
+            // console.log(dadosCNPJ)
+
+            ipt_razaoSocialEmpresa.value = dadosCNPJ.razao_social;;
+            ipt_logradouroEmpresa.value = `${dadosCNPJ.estabelecimento.tipo_logradouro} ${dadosCNPJ.estabelecimento.logradouro}`;
+            ipt_numeroEmpresa.value = dadosCNPJ.estabelecimento.numero;
+            ipt_cepEmpresa.value = dadosCNPJ.estabelecimento.cep;
+
+        })
+        .catch(error => {
+            console.log("CNPJ não localizado na base de dados!")
+        })
+
+}
+
+function disableInputs(){
+    ipt_cnpjEmpresa.value = sessionStorage.CNPJ_EMPRESA;
+        ipt_cnpjEmpresa.setAttribute('disabled', '');
+        ipt_razaoSocialEmpresa.setAttribute('disabled', '');
+        ipt_cepEmpresa.setAttribute('disabled', '');
+        ipt_logradouroEmpresa.setAttribute('disabled', '');
+        ipt_numeroEmpresa.setAttribute('disabled', '');
+        ipt_telefoneEmpresa.setAttribute('disabled', '');
+        ipt_nomeFunc.setAttribute('disabled', '');
+        ipt_cpfFunc.setAttribute('disabled', '');
+        ipt_telefoneFunc.setAttribute('disabled', '');
+        ipt_dataFunc.setAttribute('disabled', '');
+        ipt_emailFunc.setAttribute('disabled', '');
+        ipt_cargoFunc.setAttribute('disabled', '');
+        ipt_loginFunc.setAttribute('disabled', '');
+        ipt_senhaFunc.setAttribute('disabled', '');
 }
