f2dd38672fb3e9d1a5ec5e6141463a96a2b8b790
JoaoVitorFB <joao.batista@sptech.school>
2023-10-23 13:39:44 -0300

    factor: listando ultimos alertas na tabela

--- siteConWay/public/airway/js/graficoJoao.js
+++ siteConWay/public/airway/js/graficoJoao.js
@@ -6,7 +6,7 @@ function exibirRegistrosTotens() {
                 console.log("Nenhum resultado encontrado!!");
             }
             resposta.json().then(function (resposta) {
-                console.log(resposta)
+                // console.log(resposta)
             });
         } else {
             throw ('Houve um erro na API!');
@@ -24,7 +24,7 @@ function exibirRegistrosTotemID(idTotem) {
                 console.log("Nenhum resultado encontrado!!");
             }
             resposta.json().then(function (resposta) {
-                console.log(resposta)
+                // console.log(resposta)
             });
         } else {
             throw ('Houve um erro na API!');
@@ -34,15 +34,82 @@ function exibirRegistrosTotemID(idTotem) {
     });
 }
 
-function exibirUltimosAlertas(idEmpresa) {
+function exibirUltimosAlertas() {
 
-    fetch(`/graficoJoao/exibirUltimosAlertas/${idEmpresa}`).then(function (resposta) {
+    fetch(`/graficoJoao/exibirUltimosAlertas/${sessionStorage.FK_EMPRESA}`).then(function (resposta) {
         if (resposta.ok) {
             if (resposta.status == 204) {
                 console.log("Nenhum resultado encontrado!!");
             }
             resposta.json().then(function (resposta) {
+
                 console.log(resposta)
+
+                for (let i = 0; i < resposta.length; i++) {
+
+                        // Contando totens e inserindo nas KPIS
+
+
+
+                        //Inserindo totens na tabela de totens em alerta
+
+                        var tabela = document.getElementById("dataTable");
+                        var dados = resposta[i];
+
+                        var tdAeroporto = document.createElement("td");
+                        tdAeroporto.setAttribute("scope", "row");
+                        tdAeroporto.innerHTML = dados.nomeAeroporto;
+                        
+                        var tdTotem = document.createElement("td");
+                        tdTotem.setAttribute("scope", "row");
+                        tdTotem.innerHTML = dados.nomeTotem;
+
+                        var tdComponente = document.createElement("td");
+                        tdComponente.setAttribute("scope", "row");
+                        tdComponente.innerHTML = dados.nomeComponente;
+
+                        var tdValor = document.createElement("td");
+                        tdValor.setAttribute("scope", "row");
+                        tdValor.innerHTML = dados.valor;
+
+                        // var tdDataHora = document.createElement("td");
+                        // tdDataHora.setAttribute("scope", "row");
+                        // tdDataHora.innerHTML = dados.dataHora;
+
+                        var tdIndicador = document.createElement("td");
+                        tdIndicador.setAttribute("scope", "row");
+
+                        if (dados.tipoAlerta == 1){
+
+                            var iIndicador = document.createElement("td");
+                            iIndicador.setAttribute("class", "fas fa-circle text-danger");
+
+                        } else {
+
+                            var iIndicador = document.createElement("td");
+                            iIndicador.setAttribute("class", "fas fa-circle text-warning");
+
+                        }
+
+
+                        var tr = document.createElement("tr");
+                        var tbody = document.createElement("tbody");
+                        tbody.setAttribute("id", "tbodyTable")
+
+                        tr.appendChild(tdAeroporto);
+                        tr.appendChild(tdTotem);
+                        tr.appendChild(tdComponente);
+                        tr.appendChild(tdValor);
+                        // tr.appendChild(tdDataHora);
+                        tdIndicador.appendChild(iIndicador)
+                        tr.appendChild(tdIndicador);
+                        tbody.appendChild(tr)
+                        tabela.appendChild(tbody)
+                    }
+                    $(document).ready(function () {
+                        $('#dataTable').DataTable();
+                    });
+
             });
         } else {
             throw ('Houve um erro na API!');
--- siteConWay/src/models/graficoJoaoModel.js
+++ siteConWay/src/models/graficoJoaoModel.js
@@ -27,7 +27,7 @@ function exibirUltimosAlertas(idEmpresa) {
     "Acessei o graficoJoaoModel e executei a função exibirRegistrosTotens(): ",
   );
   var instrucao = `
-  SELECT C.nome, T.nome, R.valor, AR.nome, R.dataHora FROM Alerta AS A JOIN Registro AS R ON A.fkRegistro = R.idRegistro JOIN Componente AS C ON R.fkComponente = C.idComponente JOIN Totem AS T ON R.fkTotem = T.idTotem JOIN Aeroporto AS AR ON T.fkAeroporto = AR.idAeroporto WHERE dataHora = (SELECT MAX(dataHora) FROM Registro) AND fkEmpresa = ${idEmpresa};
+  SELECT C.nome AS nomeComponente, T.nome AS nomeTotem, R.valor, AR.nome AS nomeAeroporto, R.dataHora, A.tipo AS tipoAlerta FROM Alerta AS A JOIN Registro AS R ON A.fkRegistro = R.idRegistro JOIN Componente AS C ON R.fkComponente = C.idComponente JOIN Totem AS T ON R.fkTotem = T.idTotem JOIN Aeroporto AS AR ON T.fkAeroporto = AR.idAeroporto WHERE dataHora = (SELECT MAX(dataHora) FROM Registro) AND fkEmpresa = ${idEmpresa};
   `;
   console.log("Executando a instrução SQL: \n" + instrucao);
   return database.executar(instrucao);
