db58042e54de33ed417f586bf497d407c335644c
anabueno25 <125586664+anabueno25@users.noreply.github.com>
2023-11-15 21:32:27 -0300

    refactor(graficoAna) : listagem de apenas os aeroportos que estão cadastrados

--- siteConWay/public/airway/js/graficoAna.js
+++ siteConWay/public/airway/js/graficoAna.js
@@ -84,3 +84,165 @@ function plotarGraficoCrawler() {
       chart.render();
 
 }
+
+function exibirEstadosComTotens() {
+  var estado = document.getElementById("select-estado");
+  fetch(`/graficoBruno/exibirEstadosComTotens`)
+      .then(function (resposta) {
+          if (resposta.ok) {
+
+              resposta.json().then(json => {
+
+                  estado.innerHTML = "";
+
+                  let option1 = document.createElement("option");
+                  option1.innerHTML = "Todos os estados";
+                  option1.setAttribute("data-default", "");
+                  option1.setAttribute("value", "0");
+                  option1.setAttribute("selected", "");
+                  estado.appendChild(option1);
+
+                  var municipio = document.getElementById("select-municipio");
+
+                  municipio.innerHTML = "";
+                  let option2 = document.createElement("option");
+                  option2.innerHTML = "Selecione um estado...";
+                  option2.setAttribute("data-default", "");
+                  option2.setAttribute("disabled", "");
+                  option2.setAttribute("selected", "");
+                  municipio.appendChild(option2);
+
+                  var aeroporto = document.getElementById("select-aeroporto");
+                  aeroporto.removeAttribute("disabled");
+                  aeroporto.innerHTML = "";
+                  let option3 = document.createElement("option");
+                  option3.innerHTML = "Selecione um municipio...";
+                  option3.setAttribute("data-default", "");
+                  option3.setAttribute("disabled", "");
+                  option3.setAttribute("selected", "");
+                  aeroporto.appendChild(option3);
+                  aeroporto.setAttribute("disabled", '')
+
+                  for (let i = 0; i < json.length; i++) {
+                      let publicacao = json[i];
+                      let option = document.createElement("option");
+                      option.innerHTML = publicacao.estado;
+                      option.setAttribute("value", publicacao.estado);
+                      estado.appendChild(option);
+                  }
+              });
+          } else {
+              resposta.text().then(texto => {
+                  console.error(texto);
+              });
+          }
+      }).catch(function (erro) {
+          console.log(erro);
+      });
+  return false;
+}
+
+function exibirMunicipiosComTotens() {
+  var estado = document.getElementById("select-estado");
+  if (estado.value == 0) {
+      var municipio = document.getElementById("select-municipio");
+      municipio.setAttribute("disabled", "");
+
+  } else {
+      fetch(`/graficoBruno/exibirMunicipiosComTotens/${estado.value}`)
+          .then(function (resposta) {
+              if (resposta.ok) {
+
+                  resposta.json().then(json => {
+
+                      var municipio = document.getElementById("select-municipio");
+                      municipio.removeAttribute("disabled");
+                      municipio.innerHTML = "";
+                      let option1 = document.createElement("option");
+                      option1.innerHTML = "Todos os municípios";
+                      option1.setAttribute("data-default", "");
+                      option1.setAttribute("value", "0");
+                      option1.setAttribute("selected", "");
+                      municipio.appendChild(option1);
+
+                      var aeroporto = document.getElementById("select-aeroporto");
+                      aeroporto.setAttribute("disabled", '')
+                      aeroporto.innerHTML = "";
+                      let option2 = document.createElement("option");
+                      option2.innerHTML = "Selecione um município...";
+                      option2.setAttribute("data-default", "");
+                      option2.setAttribute("disabled", "");
+                      option2.setAttribute("selected", "");
+                      aeroporto.appendChild(option2);
+
+                      for (let i = 0; i < json.length; i++) {
+                          let publicacao = json[i];
+                          let option = document.createElement("option");
+                          option.innerHTML = publicacao.municipio;
+                          option.setAttribute("value", publicacao.municipio);
+                          municipio.appendChild(option);
+                      }
+                  });
+              } else {
+                  resposta.text().then(texto => {
+                      console.error(texto);
+                  });
+              }
+          }).catch(function (erro) {
+              console.log(erro);
+          });
+      return false;
+  }
+}
+
+function exibirAeroportosComTotens(municipio) {
+  var estado = document.getElementById("select-estado");
+  var municipio = document.getElementById("select-municipio");
+  if (municipio.value == 0) {
+      var aeroporto = document.getElementById("select-aeroporto");
+      aeroporto.removeAttribute("disabled");
+      aeroporto.innerHTML = "";
+      let option3 = document.createElement("option");
+      option3.innerHTML = "Selecione um municipio...";
+      option3.setAttribute("data-default", "");
+      option3.setAttribute("disabled", "");
+      option3.setAttribute("selected", "");
+      aeroporto.appendChild(option3);
+      aeroporto.setAttribute("disabled", '')
+  } else {
+      fetch(`/graficoBruno/exibirAeroportosComTotens/${municipio.value}`)
+          .then(function (resposta) {
+              if (resposta.ok) {
+                  resposta.json().then(json => {
+
+                      var aeroporto = document.getElementById("select-aeroporto");
+                      aeroporto.removeAttribute("disabled");
+                      aeroporto.innerHTML = "";
+                      let option = document.createElement("option");
+                      option.innerHTML = "Todos os aeroportos";
+                      option.setAttribute("data-default", "");
+                      option.setAttribute("value", "0");
+                      option.setAttribute("selected", "");
+                      aeroporto.appendChild(option);
+
+                      for (let i = 0; i < json.length; i++) {
+                          let publicacao = json[i];
+                          let option = document.createElement("option");
+                          option.innerHTML = publicacao.nomeAeroporto;
+                          option.setAttribute("value", publicacao.nomeAeroporto);
+                          aeroporto.appendChild(option);
+                      }
+
+                  });
+              } else {
+                  resposta.text().then(texto => {
+                      console.error(texto);
+                  });
+              }
+          }).catch(function (erro) {
+              console.log(erro);
+          });
+      return false;
+
+  }
+}
