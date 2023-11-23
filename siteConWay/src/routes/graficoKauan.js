9ba9959c40442bdf22e9975d4e0106fe3517e8bf
KauanCursino <kauan.souza@sptech.school>
2023-11-22 22:42:57 -0300

    feat(AZURE): conexão com o banco na nuvem em windows server

--- siteConWay/src/routes/graficoKauan.js
+++ siteConWay/src/routes/graficoKauan.js
@@ -1,4 +1,8 @@
  var express = require("express");
+ var router = express.Router();
 
+ router.get("/buscarTotens/:idEmpresa", function (req, res) {
+    graficoJoaoController.buscarTotens(req, res);
+});
 
 module.exports = router;
