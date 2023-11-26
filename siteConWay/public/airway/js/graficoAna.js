b8c99740e3ca55cb6c00d1ba494b2b1b3c4a138c
anabueno25 <125586664+anabueno25@users.noreply.github.com>
2023-11-26 13:46:02 -0300

    refactor(graficoAna): arrumando kpi errada

--- siteConWay/public/airway/js/graficoAna.js
+++ siteConWay/public/airway/js/graficoAna.js
@@ -39,7 +39,7 @@ function atualizarKPITotalOperacao() {
 function atualizarKPITotensPendentes(valorPendente) {
   var kpiPendente = document.getElementById("totalPendentes");
   if (kpiPendente) {
-    kpiPendente.innerText = valorPendente;
+    kpiPendente.innerText = valorPendente + 1;
   }
 }
 
