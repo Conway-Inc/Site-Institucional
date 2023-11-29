0da0bb7a4469d5fdb722e26835f8a2badf38e502
beatriz <beatriz.rocha@sptech.school>
2023-11-29 15:25:46 -0300

    feat(empresa-metricas): cadastro de métricas gerais por componente funcional

--- siteConWay/src/models/metricasModel.js
+++ siteConWay/src/models/metricasModel.js
@@ -3,7 +3,12 @@ var database = require("../database/config");
 function cadastrarMetricasCpu(alertaCpu, criticoCpu, fkEmpresa){
     console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarMetricasCpu()");
 
-    var instrucao = ``;
+    var instrucao = `
+    UPDATE TotemComponente
+    INNER JOIN Totem ON TotemComponente.fkTotem = Totem.idTotem
+    SET TotemComponente.alerta = ${alertaCpu}, TotemComponente.critico = ${criticoCpu}
+    WHERE Totem.fkEmpresa = ${fkEmpresa} AND TotemComponente.fkComponente = 1;
+    `;
 
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -12,7 +17,12 @@ function cadastrarMetricasCpu(alertaCpu, criticoCpu, fkEmpresa){
 function cadastrarMetricasMemo(alertaMemo, criticoMemo, fkEmpresa){
     console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarMetricasCpu()");
 
-    var instrucao = ``;
+    var instrucao = `
+    UPDATE TotemComponente
+    INNER JOIN Totem ON TotemComponente.fkTotem = Totem.idTotem
+    SET TotemComponente.alerta = ${alertaMemo}, TotemComponente.critico = ${criticoMemo}
+    WHERE Totem.fkEmpresa = ${fkEmpresa} AND TotemComponente.fkComponente = 2;
+    `;
 
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -21,7 +31,12 @@ function cadastrarMetricasMemo(alertaMemo, criticoMemo, fkEmpresa){
 function cadastrarMetricasDisco(alertaDisco, criticoDisco, fkEmpresa){
     console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarMetricasCpu()");
 
-    var instrucao = ``;
+    var instrucao = `
+    UPDATE TotemComponente
+    INNER JOIN Totem ON TotemComponente.fkTotem = Totem.idTotem
+    SET TotemComponente.alerta = ${alertaDisco}, TotemComponente.critico = ${criticoDisco}
+    WHERE Totem.fkEmpresa = ${fkEmpresa} AND TotemComponente.fkComponente = 3;
+    `;
 
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
