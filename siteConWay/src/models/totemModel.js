c907024bf21ea69f7a15e63e712d31e4e3bb0353
brunolimabh <bruno.lima@sptech.school>
2023-12-01 10:32:50 -0300

    cleanup: Corrigindo rota totemModel.js

--- siteConWay/src/models/totemModel.js
+++ siteConWay/src/models/totemModel.js
@@ -1,20 +1,19 @@
 var database = require("../database/config");
 
 function exibirTabelaTotem(idEmpresa) {
-  console.log("ACESSEI O TOTEM  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function exibirTabelaTotem()");
-    var instrucao = `
+  console.log(`Acessei o totemModel.js, executei exibirTabelaTotem(${idEmpresa})`);
+  var instrucao = `
     SELECT tot.idTotem, tot.nome, aro.nome AS aeroportoTotem, emp.nome AS empresaTotem
     FROM Totem tot INNER JOIN aeroporto aro ON tot.fkAeroporto = aro.idAeroporto INNER JOIN empresa emp
     ON tot.fkEmpresa = emp.idEmpresa WHERE tot.fkEmpresa = ${idEmpresa};
     `;
-    console.log("Executando a instrução SQL: \n" + instrucao);
-    return database.executar(instrucao);
+  console.log("Executando a instrução SQL: \n" + instrucao);
+  return database.executar(instrucao);
 }
 
 function exibirMunicipios(estado) {
-  console.log(
-    "ACESSEI O graficoBrunoModel \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function exibirMunicipios(): ",estado
-  );
+  console.log(`Acessei o totemModel.js, executei exibirMunicipios(${estado})`);
+
   var instrucao = `
     SELECT municipio FROM vw_aeroportos WHERE estado = '${estado}' GROUP BY municipio ORDER BY municipio;
   `;
@@ -23,9 +22,7 @@ function exibirMunicipios(estado) {
 }
 
 function exibirAeroportos(municipio) {
-  console.log(
-    "ACESSEI O graficoBrunoModel \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function exibirMunicipios(): ",municipio
-  );
+  console.log(`Acessei o totemModel.js, executei exibirAeroportos(${municipio})`);
   var instrucao = `
     SELECT idAeroporto, nome FROM vw_aeroportos WHERE municipio = '${municipio}';
   `;
@@ -33,18 +30,21 @@ function exibirAeroportos(municipio) {
   return database.executar(instrucao);
 }
 
-function cadastrarTotem(nomeTotem, fkAeroporto, fkEmpresa){
-  console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarTotem()");
-  var instrucao = `
-  EXEC cadastrarTotem('${nomeTotem}', '${fkAeroporto}', '${fkEmpresa}' );
-  
-  `;
+function cadastrarTotem(nomeTotem, fkAeroporto, fkEmpresa) {
+  console.log(`Acessei o totemModel.js, executei cadastrarTotem(${nomeTotem},${fkAeroporto},${fkEmpresa})`);
+  if (process.env.AMBIENTE_PROCESSO == "producao") {
+    var instrucao = `
+    EXEC cadastrarTotem('${nomeTotem}', '${fkAeroporto}', '${fkEmpresa}' );`;
+  } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+    var instrucao = `
+    CALL cadastrarTotem('${nomeTotem}', '${fkAeroporto}', '${fkEmpresa}' );`;
+  }
   console.log("Executando a instrução SQL: \n" + instrucao);
   return database.executar(instrucao);
 }
 
-function criarViewTotem(idTotem){
-  console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarComponente()");
+function criarViewTotem(idTotem) {
+  console.log(`Acessei o totemModel.js, executei criarViewTotem(${idTotem})`);
   var instrucao = `
     CREATE VIEW vw_totem${idTotem} AS SELECT * FROM vw_RegistroEstruturado WHERE idTotem = ${idTotem};
   `;
@@ -52,21 +52,26 @@ function criarViewTotem(idTotem){
   return database.executar(instrucao);
 }
 
-function cadastrarComponente(componente){
-  console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarComponente()");
-  var instrucao = `
-  EXEC cadastrar_maquinaComponente (${componente})
-  `;
+function cadastrarComponente(componente) {
+  console.log(`Acessei o totemModel.js, executei cadastrarComponente(${componente})`);
+  if (process.env.AMBIENTE_PROCESSO == "producao") {
+    var instrucao = `
+    EXEC cadastrar_maquinaComponente (${componente});`;
+  } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+    var instrucao = `
+    CALL cadastrar_maquinaComponente (${componente});`;
+  }
   console.log("Executando a instrução SQL: \n" + instrucao);
   return database.executar(instrucao);
 }
 
-function cadastrarMetricas(){
-  console.log("ACESSEI O HARDWARE MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrarTotem()");
+function cadastrarMetricas() {
+  console.log(`Acessei o totemModel.js, executei cadastrarMetricas()`);
   var instrucao = `
   SELECT idComponente * FROM TotemComponete;
-  INSERT INTO TotemComponete(valor, alerta, critico, fkComponente, fkTotem) VALUES()
-  `
+  INSERT INTO TotemComponete(valor, alerta, critico, fkComponente, fkTotem) VALUES();`
+  console.log("Executando a instrução SQL: \n" + instrucao);
+  return database.executar(instrucao);
 }
 
 
